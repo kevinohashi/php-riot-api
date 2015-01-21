61a3fb32ed23ee11bdb52c507d0818136a2222b3
Supericy <koise150@hotmail.com>
2015-01-21 06:03:39 -0600

    Comment wording

--- CacheInterface.php
+++ CacheInterface.php
@@ -3,21 +3,21 @@
 interface CacheInterface {
 
 	/**
-	 * @param string $key Check if the cache contains data for the specified key
+	 * @param string $key Checks whether or not the cache contains unexpired data for the specified key
 	 * @return bool
 	 */
 	public function has($key);
 
 	/**
 	 * @param string $key Gets data for specified key
-	 * @return string|null Returns null if the cache has expired or non-existing data
+	 * @return string|null Returns null if the cached item doesn't exist or has expired
 	 */
 	public function get($key);
 
 	/**
 	 * @param string $key
 	 * @param $data
-	 * @param int $ttl Time for the data to live inside the cache
+	 * @param int $ttl Time in seconds before the data becomes expired
 	 * @return mixed
 	 */
 	public function put($key, $data, $ttl = 0);
